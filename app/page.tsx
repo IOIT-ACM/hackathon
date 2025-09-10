ea5fefb1c3d8978e12b899b3edcceb30b78099fa
Sarvesh Kolte <renarinkholin4@proton.me>
2025-09-10 22:20:44 +0530

    feat: add discord link on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -31,13 +31,22 @@ export default function Home() {
         z-10 px-10'>
           <h1 className={cn('text-center font-bold text-7xl  text-shadow-[0_35px_35px_rgb(0_0_0_/_0.25)] text-shadow-2xl  text-[#05BE2B]', vt323.className)}>The Matrix <span className="animate-blink">_</span></h1>
           <p className={cn('text-center text-4xl  text-white', vt323.className)}>Plug into The Matrix, where code bends reality.<br /> Create what the future dares to imagine.</p>
-          <a className="relative cursor-pointer mt-2 px-10 py-2 text-lg font-medium text-black bg-[#141710] mx-auto hover:bg-primary-white hover:px-20 transition-all" href={process.env.REGISTRATION_LINK || ""}>
-            <p className={cn("text-2xl text-white mx-auto text-center", vt323.className)} >Register Now</p>
-            {/* Left bracket */}
-            <span className="absolute left-0 top-0 h-full w-3 border-l-3 border-t-3 border-b-3 border-primary-white"></span>
-            {/* Right bracket */}
-            <span className="absolute right-0 top-0 h-full w-3 border-r-3 border-t-3 border-b-3 border-primary-white"></span>
-          </a>
+          <div className="flex flex-row items-center justify-center gap-4">
+            <a className="relative cursor-pointer mt-2 px-10 py-2 text-lg font-medium text-black bg-[#141710]  hover:bg-primary-white hover:px-20 transition-all" href={process.env.REGISTRATION_LINK || ""}>
+              <p className={cn("text-2xl text-white mx-auto text-center", vt323.className)} >Register Now</p>
+              {/* Left bracket */}
+              <span className="absolute left-0 top-0 h-full w-3 border-l-3 border-t-3 border-b-3 border-primary-white"></span>
+              {/* Right bracket */}
+              <span className="absolute right-0 top-0 h-full w-3 border-r-3 border-t-3 border-b-3 border-primary-white"></span>
+            </a>
+            <a className="relative cursor-pointer mt-2 px-10 py-2 text-lg font-medium text-black bg-[#141710]  hover:bg-primary-white hover:px-20 transition-all" target="_blank" href={"https://discord.gg/dkVV2VDw"}>
+              <p className={cn("text-2xl text-white mx-auto text-center", vt323.className)} >Discord</p>
+              {/* Left bracket */}
+              <span className="absolute left-0 top-0 h-full w-3 border-l-3 border-t-3 border-b-3 border-primary-white"></span>
+              {/* Right bracket */}
+              <span className="absolute right-0 top-0 h-full w-3 border-r-3 border-t-3 border-b-3 border-primary-white"></span>
+            </a>
+          </div>
 
         </div>
       }
